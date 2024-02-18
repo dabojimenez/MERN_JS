1bbbe5d8ff986e8abe2eab725250602be34ad6e1
dabojimenez <dabojimenez16@hotmail.com>
2024-02-17 20:42:52 -0500

    Se genera un token randomico
    Ademas se crea la carpeta helpers, para poder reutilizarle y ademas creamos una nueva ruta para poder cofirmar el token
    Adicional se modifica el token una ves que envia el token y colocandole null y confirmado  su autenticación

--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,6 +1,6 @@
 // En este archivo van todas las rutas que esten relacionadas a veterinario
 import express from 'express';
-import { perfil, registrar } from '../controllers/veterinarioController.js';
+import { confirmar, perfil, registrar } from '../controllers/veterinarioController.js';
 
 // iniciamos el ruteo
 const router = express.Router();
@@ -8,5 +8,7 @@ const router = express.Router();
 router.post('/', registrar);
 
 router.get('/perfil', perfil);
+
+router.get('/confirmar/:token', confirmar); // con los dos puntos, pasamos un parametro dinámico
 
 export default router;
