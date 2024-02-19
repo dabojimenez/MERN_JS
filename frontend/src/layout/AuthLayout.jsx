aa4ef4164d96d99fb8362df8afc6b1cdc27014fe
dabojimenez <dabojimenez16@hotmail.com>
2024-02-18 20:04:17 -0500

    Se crea el layout del login, con enlaces de registrar y olvide mi password

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -3,14 +3,16 @@
 import { Outlet } from "react-router-dom";
 
 export const AuthLayout = () => {
-  return (
-    // de esta forma es un fragment y no generara codigo html
-    <>
-        <h1>Administrar de Pacientes de Veterinario</h1>
-        {/* Muestra el contenido de lo que tiene como hijos*/}
-        <Outlet />
-    </>
-  )
+    return (
+        // de esta forma es un fragment y no generara codigo html
+        <>
+            <main className='container mx-auto md:grid md:grid-cols-2 mt-12 gap-10 p-5 items-center'>
+                {/* Muestra el contenido de lo que tiene como hijos*/}
+                <Outlet />
+            </main>
+
+        </>
+    )
 };
 
 export default AuthLayout;
