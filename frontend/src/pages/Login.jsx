370dc020434eff26400331d741b97a4ee764cf57
dabojimenez <dabojimenez16@hotmail.com>
2024-02-25 16:52:30 -0500

    Se autentica al usuario con JWT que nos envia el backend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,46 @@
-import { Link } from 'react-router-dom'; // importamos este componente, para evitar el spiner de carga al presionar en un link (NavLink) es lo mismo
-import useAuth from '../hooks/useAuth';
+import { Link, useNavigate } from 'react-router-dom'; // importamos este componente, para evitar el spiner de carga al presionar en un link (NavLink) es lo mismo
+// import useAuth from '../hooks/useAuth';
+import { useState } from 'react';
+import Alerta from '../components/Alerta';
+import clienteAxios from '../config/axios';
 
 const Login = () => {
-  const { auth } = useAuth(); // llamamos la función y obtenemos los valores que definimos en el contextprovider
-  console.log(auth);
+  // const { auth } = useAuth(); // llamamos la función y obtenemos los valores que definimos en el contextprovider
+  // console.log(auth);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alerta, setAlerta] = useState({});
+
+  const navigate = useNavigate(); // para poder redirigir al usuario
+
+  // Funciones handle
+  const handleSubmit = async e => {
+    e.preventDefault();
+    
+    if ([email, password].includes('')) {
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+      });
+      return;
+    }
+
+    try {
+      const { data } = await clienteAxios.post('/veterinarios/login', {email, password});
+      // Almacenamso el token en localstorage
+      localStorage.setItem('token', data.token)
+      // le navegamos al usuario
+      navigate('/admin');// funcion que toma una ruta para redireccionar
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      })
+    }
+  }
+
+  const { msg } = alerta;
   return (
     <>
 
@@ -12,7 +49,11 @@ const Login = () => {
       </div>
 
       <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-        <form>
+        {/* Mostramos el mensaje */}
+        {msg && <Alerta alerta={alerta}/>}
+        <form
+          onSubmit={handleSubmit}
+        >
           <div className='my-5'>
             <label
               className='uppercase text-gray-600 block text-xl font-bold'>
@@ -22,6 +63,8 @@ const Login = () => {
               type='email'
               placeholder='Email de Registro'
               className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
             />
           </div>
           <div className='my-5'>
@@ -33,6 +76,8 @@ const Login = () => {
               type='password'
               placeholder='Tu Password'
               className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
             />
           </div>
 
