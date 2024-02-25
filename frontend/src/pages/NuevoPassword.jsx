d1d39b92594462c0cbca3d1fbc58816fb34adcb6
dabojimenez <dabojimenez16@hotmail.com>
2024-02-25 11:40:05 -0500

    Se finaliza el layout de olvide password y se envia el correo electronico

---
+++ frontend/src/pages/NuevoPassword.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Alerta from '../components/Alerta';
+import clienteAxios from '../config/axios';
+
+const NuevoPassword = () => {
+  const [password, setPassword] = useState('');
+  const [alerta, setAlerta] = useState({});
+  const [tokenValido, setTokenValido] = useState(false);
+
+  // Parametros de la url
+  const params = useParams();
+  const { token } = params;
+
+  // Se ejecutara cunado el componente cargue
+  useEffect(() => {
+    const comprobarToken = async () => {
+      try {
+        await clienteAxios(`/veterinarios/olvide-password/${token}`);
+        setAlerta({
+          msg: 'Coloca tu Nuevo Password'
+        });
+        setTokenValido(true);
+      } catch (error) {
+        setAlerta({
+          msg: 'Hubo un error con el enlace',
+          error: true
+        })
+      }
+    }
+    comprobarToken();
+  }, []);
+
+  // extraemos el mensaje
+  const { msg } = alerta;
+  return (
+    <>
+      <div>
+        <h1 className='text-indigo-600 font-black text-6xl'>Restablece tu Password y no Pierdas Acceso a  <span className='text-black'>tus Pacientes</span> </h1>
+      </div>
+
+      <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+
+        {/* Mensaje de retroalimentacion al usuario */}
+        {msg && <Alerta alerta={alerta} />}
+
+        {/* Confirmamos el token si es valido, si es vlaido retornamos el form */}
+        {
+          tokenValido &&
+          <form
+          >
+
+            <div className='my-5'>
+              <label
+                className='uppercase text-gray-600 block text-xl font-bold'>
+                Nuevo Password
+              </label>
+              <input
+                type='password'
+                placeholder='Tu Nuevo Password'
+                className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+
+            <input
+              type='submit'
+              value={'Guardar Nuevo Password'}
+              className='bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto '
+            >
+            </input>
+          </form>
+        }
+
+      </div>
+    </>
+  )
+}
+
+export default NuevoPassword
