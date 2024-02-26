2c1a3513091de8e3254386cbdaed47c91ba9e316
dabojimenez <dabojimenez16@hotmail.com>
2024-02-25 19:49:09 -0500

    Se agrega el disenio del formulario para el futuro registro de información

--- frontend/src/pages/AdministrarPacientes.jsx
+++ frontend/src/pages/AdministrarPacientes.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useState } from 'react'
+import Formulario from '../components/Formulario'
+import ListadoPacientes from '../components/ListadoPacientes'
+
 
 function AdministrarPacientes() {
+  const [mostrarFormulario, setMostrarFormulario] = useState(false);
   return (
-    <div>AdministrarPacientes</div>
+    <div className='flex flex-col md:flex-row'>
+      {/* Formulario de pacientes */}
+
+      <button
+        type='button'
+        className='bg-indigo-600 text-white font-bold uppercase mx-10 p-3 rounded-md mb-10 md:hidden'
+        onClick={() => setMostrarFormulario(!mostrarFormulario)}
+      >
+        {mostrarFormulario ? 'Ocultar Formulario' : 'Mostrar Formulario'}
+      </button>
+      {/* le agregamos el siguiente codigo, para mostrar o ocultar el formulario */}
+      <div className={`${mostrarFormulario ? 'block' : 'hidden' } md:block md:w-1/2 lg:w-2/5`}>
+        <Formulario/>
+      </div>
+      {/* Fin Formulario de pacientes */}
+
+
+      {/* Listado pacientes */}
+      <div className='md:w-1/2 lg:w-3/5'>
+        <ListadoPacientes/>
+      </div >
+      {/* Fin Listado pacientes */}
+
+    </div>
   )
 }
 
