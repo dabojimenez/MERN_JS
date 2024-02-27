ed5e5f4f5cfd592b077128ff1ecd906594d20b34
dabojimenez <dabojimenez16@hotmail.com>
2024-02-26 21:21:49 -0500

    Se crea y se agrega el provider de pacientes en el App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import NuevoPassword from "./pages/NuevoPassword";
 import { AuthProvider } from './context/AuthProvider';
 import RutaProtegida from "./layout/RutaProtegida";
 import AdministrarPacientes from "./pages/AdministrarPacientes";
+import { PacientesProvider } from "./context/PacientesProvider";
 
 function App() {
 
@@ -22,34 +23,36 @@ function App() {
     <BrowserRouter>
       {/* // Debemos rodear todo con el provider o el contexto */}
       <AuthProvider>
-        <Routes>
-          {/* Route, lo podmeos definir como ciertos layouts para determinado grupo de páginas. Podemos definir cierto diseno en comun */}
-          {/* element => va a ser la pagina principal y cargara lo que se le pasa en el element*/}
-          {/* Route, nos permite agrupar lo que este relacionado con la autenticacion */}
-          {/* AuthLayout, es el componente padre y sus hijos pueden tener su diseno individial, biene a ser como el master pages */}
-          <Route path="/" element={<AuthLayout />}>
-            {/* index, le dice que es el primer componente osea el primer componete/ruta que se mostrara */}
-            <Route index element={<Login />} />
-            <Route path="olvide-password" element={<OlvidePassword />} />
-            <Route path="olvide-password/:token" element={<NuevoPassword />} />
-            <Route path="registrar" element={<Registrar />} />
-            <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
-          </Route>
+        <PacientesProvider>
+          <Routes>
+            {/* Route, lo podmeos definir como ciertos layouts para determinado grupo de páginas. Podemos definir cierto diseno en comun */}
+            {/* element => va a ser la pagina principal y cargara lo que se le pasa en el element*/}
+            {/* Route, nos permite agrupar lo que este relacionado con la autenticacion */}
+            {/* AuthLayout, es el componente padre y sus hijos pueden tener su diseno individial, biene a ser como el master pages */}
+            <Route path="/" element={<AuthLayout />}>
+              {/* index, le dice que es el primer componente osea el primer componete/ruta que se mostrara */}
+              <Route index element={<Login />} />
+              <Route path="olvide-password" element={<OlvidePassword />} />
+              <Route path="olvide-password/:token" element={<NuevoPassword />} />
+              <Route path="registrar" element={<Registrar />} />
+              <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+            </Route>
 
-          {/* Podremos tener un dasbor totalmente diferente al del area publica de esta forma */}
-          {/* <Route path="/admin" element={<AuthLayout/>}>
+            {/* Podremos tener un dasbor totalmente diferente al del area publica de esta forma */}
+            {/* <Route path="/admin" element={<AuthLayout/>}>
           <Route index element={<Login/>} />
           <Route path="olvide-password" element={<OlvidePassword/>}/>
           <Route path="registrar" element={<Registrar/>}/>
           <Route path="confirmar/:id" element={<ConfirmarCuenta/>}/>
         </Route> */}
 
-        {/* Definimso otro grupo de rutas que seran protegidas/privadas */}
-        <Route path="/admin" element={<RutaProtegida/>}>
-          <Route index element={<AdministrarPacientes />} />
-        </Route>
+            {/* Definimso otro grupo de rutas que seran protegidas/privadas */}
+            <Route path="/admin" element={<RutaProtegida />}>
+              <Route index element={<AdministrarPacientes />} />
+            </Route>
 
-        </Routes>
+          </Routes>
+        </PacientesProvider>
       </AuthProvider>
     </BrowserRouter>
   )
---
+++ frontend/src/context/PacientesProvider.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState, useEffect} from 'react';
+import clienteAxios from '../config/axios';
+
+const PacientesContext = createContext();
+
+const PacientesProvider = ({children}) => {
+
+    return(
+        <PacientesContext.Provider
+            value={{
+                
+            }}
+        >
+            {children}
+        </PacientesContext.Provider>
+    )
+}
+
+export {
+    PacientesProvider,
+}
+
+export default PacientesContext;
